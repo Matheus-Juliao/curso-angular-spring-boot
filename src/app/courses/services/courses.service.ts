41fb31f9c2e7a1a79a9b53ee6e68cd42b42da2c7
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-01-09 12:55:54 -0300

    Aula 16 - Banco de dados H2 e conexão com Angular na API Spring

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { first, tap } from 'rxjs';
+import { first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly url: string = '/assets/courses.json';
+  private readonly url: string = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
   //get<Course[]>- O operador diamante <> é utilizado pra colocar o tipo de de retorno da chamada. No caso o retorno é um Observable de Array de cursos
   //O pipe siginifica cano e serve para manipular a informação antes de retorna-lá de maneira reativa. Os dados passam pelo cano e você pode manipular o resultado final.
-  //take(1) é utilizado para fechar a inscrição depois de pegar a responsta
+  //take(1) é utilizado para fechar a inscrição depois de pegar a resposta
   //tap nós consguimos utilizar os dados para verificar o que está vindo... Ex: utilizamos o console.log para visualizar os dados no inspecionar
   //first serve pra pegar a primeira responsa do servidor e finaliza a inscrição no servidor
   public list() {
