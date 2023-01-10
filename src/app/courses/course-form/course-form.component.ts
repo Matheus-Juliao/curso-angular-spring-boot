50b14f8ec1cd41339fdbe0c18e8c2baf8fb0e9af
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-01-10 18:35:43 -0300

    Aula 21 - API Spring: Criar Curso (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
       this.form = this.formBuilder.group({
         name: [null],
@@ -31,13 +33,18 @@ export class CourseFormComponent implements OnInit {
   public onSubmit() {
     this.service.save(this.form.value)
       .subscribe({
-        next: result => console.log(result),
+        next: result => this.onSuccess(),
         error: error => this.onError()
       })
   }
 
   public onCancel() {
+    this.location.back()
+  }
 
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso.', '', { duration: 5000 })
+    this.onCancel()
   }
 
   private onError() {
